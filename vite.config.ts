a09421993a20065a1b317bac12eeb35f87b922ba
Saurabh <75977842+SaurabhHimral@users.noreply.github.com>
2025-07-26 11:50:26 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: "::",
-    port: 8080,
-  },
   base: "/Portfolio/",
   plugins: [react()],
   resolve: {
